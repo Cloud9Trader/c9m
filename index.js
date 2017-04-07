564e2de352fbefd2d85319f8bfa74b86b89d68a2
Jon Kafton <jonkafton@gmail.com>
2017-04-07 07:56:58 +0100

    Initialize via method call. Update auth key version

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const Device = require('./lib/Device');
 
-const initialize = (config) => {
-  const accessKey = config.accessKey || process.env.C9M_ACCESS_KEY;
-  if (!accessKey) {
-    console.error('Cloud9Metrics access key must be set on environment as C9M_ACCESS_KEY or supplied as config.accessKey');
-    return;
-  }
-  return new Device({
-    accessKey
-  });
-};
+const device = new Device();
+device.Device = Device;
 
-initialize.Device = Device;
-
-module.exports = initialize;
+module.exports = device; 
